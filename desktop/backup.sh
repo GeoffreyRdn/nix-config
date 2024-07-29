0158dafa77623fe8e6a961c00fe567f714164188
Geoffrey Redon <geoffreyredon@gmail.com>
2024-07-29 12:19:00 +0200

    chore: desktop backup on Mon Jul 29 12:19:00 PM CEST 2024

--- desktop/backup.sh
+++ desktop/backup.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
 mkdir -p etc/nixos
+
 mkdir -p home/geoffrey
+mkdir -p home/geoffrey/.config
 
 cp /etc/nixos/configuration.nix etc/nixos/
 
-cp /home/geoffrey/.vimrc home/geoffrey/.vimrc
-cp /home/geoffrey/.zshrc home/geoffrey/.zshrc
+cp /home/geoffrey/.vimrc home/geoffrey/
+cp /home/geoffrey/.zshrc home/geoffrey/
+cp /home/geoffrey/.config/alacritty.toml home/geoffrey/.config/
 
-cp -r /home/geoffrey/.config home/geoffrey/.config/
-cp -r /home/geoffrey/.vim home/geoffrey/.vim/
-cp -r /home/geoffrey/.wallpapers home/geoffrey/.wallpapers/
+cp -r /home/geoffrey/.config/i3 home/geoffrey/.config
+cp -r /home/geoffrey/.config/polybar home/geoffrey/.config
+cp -r /home/geoffrey/.config/keepassxc home/geoffrey/.config
+
+cp -r /home/geoffrey/.vim home/geoffrey/
+cp -r /home/geoffrey/.wallpapers home/geoffrey/
 
 git add .
 git commit -m "chore: desktop backup on $(date)"
