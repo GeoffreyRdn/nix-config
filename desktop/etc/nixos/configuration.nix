97a57716fdbac963c1340ebaa53b05e216c1946a
Geoffrey Redon <geoffrey.redon@epita.fr>
2025-01-25 09:39:20 +0100

    chore: desktop backup on Sat Jan 25 09:39:20 AM CET 2025

--- desktop/etc/nixos/configuration.nix
+++ desktop/etc/nixos/configuration.nix
@@ -61,7 +61,6 @@
     i18n.defaultLocale = "en_US.UTF-8";
     console = {
         font = "Lat2-Terminus16";
-        #keyMap = "us";
         useXkbConfig = true; # use xkbOptions in tty.
     };
 
